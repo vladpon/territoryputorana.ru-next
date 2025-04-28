380d39675fb85f9fd22e4237f264eada4ffd1364
Vladislav Ponomarev <2874787@gmail.com>
2025-04-28 22:31:33 +0700

    nig cards section button

--- src/app/Components/BigCards/BigCards.jsx
+++ src/app/Components/BigCards/BigCards.jsx
@@ -1,14 +1,13 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import { redirect, useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 import styles from './BigCards.module.scss'
-import Link from 'next/link'
-// import { useLocation, useNavigate } from 'react-router-dom'
+
 
 
 const BigCards = (props) => {
-    const { content,  sections = new Map(), imgFitContain } = props
+    const { content,  sections, imgFitContain } = props
     const [filteredContent, setFilteredContent] = useState(content)
     const [activeSection, setActiveSection] = useState('')
 
@@ -17,10 +16,11 @@ const BigCards = (props) => {
         sections && setFilteredContent(content.filter( (item) => activeSection.includes(item.section)))                
     }, [activeSection])
 
+    useEffect( () => {
+        sections && setActiveSection(sections[0])
+    }, [])
 
     const handleSectionButton = (section) => 
-        // (activeSections.includes(section)) ? setActiveSections(activeSections.filter( item => item !== section))
-        //     : setActiveSections([...activeSections, section])
         setActiveSection(section)
 
 
@@ -28,24 +28,22 @@ const BigCards = (props) => {
     <div className = {styles["big-cards"]}>
         {sections && (
                         <div className= {styles['big-cards__sections']}>
-                            {sections.forEach((key, value) => (
-                                <button  key={key} onClick={() => handleSectionButton(key)} className = {activeSection.includes(key) ? 'active main-button' : 'main-button'}>{value}</button>
-                            )
-                                )
+                            {sections.map((value) => (
+                                <button  key={value} onClick={() => handleSectionButton(value)} className = {activeSection.includes(value) ? `${styles.active} ${styles['main-button']}`  : styles['main-button']}>{value}</button>
+                                ))
                             }
                         </div>)}
         { filteredContent && (
             filteredContent.map( ( item, index ) => 
                 <div onClick = {() => redirect(item.href)} key = {index}>
                     <div className = {styles["big-cards__card"]}>
-                        <div className = {`${styles["big-cards__img"]} ${imgFitContain && styles['big-cards__img_partners']}`}>                                  
+                        <div className = {`${styles["big-cards__img"]} ${imgFitContain && styles['big-cards__img_partners']}`}>
                             <img src={item.smallImg} alt="" />
                         </div>
                         <div className = {styles["big-cards__description"]}>
                             <h3>{item.title}</h3>
                             {item.description.map( (p, index) => <p key = {index}>{p}</p>)}
-                            
-                            {item.contacts && (      
+                            {item.contacts && (
                                 <p>                                  
                                 {item.contacts.tel && (<>Контакты<br />т. <a href={`tel:${item.contacts.tel}`}>{item.contacts.tel}</a></>)}
                                 {item.contacts.email && (<><br />e-mail: <a href={`mailto:${item.contacts.email}`}>{item.contacts.email}</a></>)}
@@ -56,20 +54,12 @@ const BigCards = (props) => {
                                 </p>
                             )
                             }  
-                            
+
                             <div className={styles["big-cards__info"]}>
                                 <div className={styles["big-cards__cost"]}>
                                     <span>{item.price}</span>
                                     <span>{item.time}</span>
                                 </div>
-                                {/* {(item.varPrice !== "") && (
-                                    <div className='big-cards__cost'>
-                                        <span>{item.varPrice}</span>
-                                        <span>{item.varTime}</span>
-                                    </div>
-                                )} */}
-
-
                             </div>
                         </div>
                     </div>
