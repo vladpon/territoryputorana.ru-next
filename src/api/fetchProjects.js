d3ab9495df2b4100cef6e105e8e8e8a8406fae6c
unknown <2874787@gmail.com>
2025-04-28 11:53:54 +0700

    arctic expeditions

--- src/api/fetchProjects.js
+++ src/api/fetchProjects.js
@@ -1,5 +1,5 @@
 export async function fetchProjects() {
-    const res = await fetch('http://192.168.1.200:3000/data/projects.json')
+    const res = await fetch('http://10.6.7.17:3000/data/projects.json')
     const tours = await res.json()
     return tours  
   }
--- src/app/Components/BigCards/BigCards.jsx
+++ src/app/Components/BigCards/BigCards.jsx
@@ -59,17 +59,17 @@ const BigCards = (props) => {
                             <h3>{item.title}</h3>
                             {item.description.map( (p, index) => <p key = {index}>{p}</p>)}
                             
-                                {item.contacts && (      
-                                    <p>                                  
-                                    {item.contacts.tel && (<>Контакты<br />т. <a href={`tel:${item.contacts.tel}`}>{item.contacts.tel}</a></>)}
-                                    {item.contacts.email && (<><br />e-mail: <a href={`mailto:${item.contacts.email}`}>{item.contacts.email}</a></>)}
-                                    {item.contacts.site && (<><br /><a href={`${item.contacts.site}`}>{item.contacts.site}</a></>)}
-                                    {item.contacts.wa && (<><a href={`https://wa.me/${item.contacts.wa}`}>{'whatsapp: '+item.contacts.wa}</a></>)}
-                                    {item.contacts.address && (<><br />{`${item.contacts.address}`}</>)}
-                                    {item.files && item.files.map( (file, index) => (<a className = {styles['big-cards__filelink']} key = {index} href={`${file.src}`} style={{textDecoration: "underline"}} >{file.name}</a>))}
-                                    </p>
-                                )
-                                }  
+                            {item.contacts && (      
+                                <p>                                  
+                                {item.contacts.tel && (<>Контакты<br />т. <a href={`tel:${item.contacts.tel}`}>{item.contacts.tel}</a></>)}
+                                {item.contacts.email && (<><br />e-mail: <a href={`mailto:${item.contacts.email}`}>{item.contacts.email}</a></>)}
+                                {item.contacts.site && (<><br /><a href={`${item.contacts.site}`}>{item.contacts.site}</a></>)}
+                                {item.contacts.wa && (<><a href={`https://wa.me/${item.contacts.wa}`}>{'whatsapp: '+item.contacts.wa}</a></>)}
+                                {item.contacts.address && (<><br />{`${item.contacts.address}`}</>)}
+                                {item.files && item.files.map( (file, index) => (<a className = {styles['big-cards__filelink']} key = {index} href={`${file.src}`} style={{textDecoration: "underline"}} >{file.name}</a>))}
+                                </p>
+                            )
+                            }  
                             
                             <div className={styles["big-cards__info"]}>
                                 <div className={styles["big-cards__cost"]}>
