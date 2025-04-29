4b9d48e679fbba66c91d9b75cf7eb4bc0054b6cc
unknown <2874787@gmail.com>
2025-04-29 15:57:27 +0700

    lst

--- src/api/fetchProjects.js
+++ src/api/fetchProjects.js
@@ -1,5 +1,5 @@
 export async function fetchProjects() {
-    const res = await fetch('http://10.6.7.17:3000/data/projects.json')
+    const res = await fetch('http://localhost:3000/data/projects.json')
     const tours = await res.json()
     return tours  
   }
--- src/api/fetchTours.js
+++ src/api/fetchTours.js
@@ -1,5 +1,5 @@
 export async function fetchTours() {
-    const res = await fetch('http://10.6.7.17:3000/data/tours.json')
+    const res = await fetch('http://localhost:3000/data/tours.json')
     const tours = await res.json()
     return tours  
   }
