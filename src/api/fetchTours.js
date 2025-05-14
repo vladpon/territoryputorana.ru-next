57c6b2f91816cc5077f4f9e3f47a0340437eacb9
vlad <2874787@gmail.com>
2025-05-14 17:53:13 +0700

    lst

--- src/api/fetchTours.js
+++ src/api/fetchTours.js
@@ -1,5 +1,5 @@
 export async function fetchTours() {
-    const res = await fetch('http://192.168.1.200:3000/data/tours.json')
+    const res = await fetch(process.env.API_URL + '/data/tours.json')
     const tours = await res.json()
     return tours  
   }
