e8cffe5487aebc36b51e11e6118881e1dbecd3e3
unknown <2874787@gmail.com>
2025-09-30 17:26:40 +0700

    infoframe included came details

--- src/app/Components/InfoFrame/InfoFrame.jsx
+++ src/app/Components/InfoFrame/InfoFrame.jsx
@@ -11,12 +11,10 @@ const InfoFrame = (props) => {
                 {reference && (
                     <div className = {styles['infoframe__note']}><span>*{reference}</span></div>
                 )}
-                {included && <div className = {`${styles['infoframe__note']} ${styles['infoframe__note_included']}`}>
-                        <span>*Что включено?</span>
-                        <div className = {`${styles['infoframe__modal']} ${styles['display-switch']}`}>
-                            <div className = {styles['modal__text']} dangerouslySetInnerHTML={{__html: included}}></div>
-                        </div>
-                    </div>}
+                {included && <details>
+                                <summary className = {`${styles['infoframe__note']} ${styles['infoframe__note_included']}`}><span>*Что включено?</span></summary>
+                                <div className = {styles['modal__text']} dangerouslySetInnerHTML={{__html: included}}></div>
+                            </details>}
                 {clothes && <details>
                                 <summary className = {`${styles['infoframe__note']} ${styles['infoframe__note_included']}`}><span>*Рекомендации по одежде и снаряжению</span></summary>
                                 <div className = {styles['modal__text']} dangerouslySetInnerHTML={{__html: clothes}}></div>
