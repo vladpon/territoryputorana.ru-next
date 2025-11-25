39542c2634db172cb344a815e395ddecc44e228c
unknown <2874787@gmail.com>
2025-11-25 19:32:42 +0700

    main page

--- src/app/(MainPage)/page.jsx
+++ src/app/(MainPage)/page.jsx
@@ -2,10 +2,7 @@ import Cover from "./Cover";
 import TextWord from "./TextWord"
 import CardsBlock from './CardsBlock'
 import TextOneImage from './TextOneImage'
-import OneCardBlock from './OneCardBlock'
 import VideoBlock from './VideoBlock'
-import TextFewImages from './TextFewImages'
-import TextOneImage2 from './TextOneImage2'
 import RequestBlock from '../Components/RequestBlock/RequestBlock'
 import styles from './MainPage.module.scss'
 import { getTours } from "@/lib/mongo/tours";
@@ -22,13 +19,14 @@ const textOneImage = {
   title_h2: 'Территория Путорана',
   title_h3: 'Покори Север с нами!',
   text: [
-    'Более 10 лет мы занимаемся развитием туризма на плато Путорана. Это наше любимое дело, увлечение и постоянные открытия! Все члены нашей команды родились и выросли на Таймыре, и мы знаем, чем удивить самого искушенного путешественника. Мы покажем вам самые известные достопримечательности и места, где практически не ступала нога человека.',
-    'Каждый сезон наша команда разрабатывает новые зимние и летние маршруты, уделяя особое внимание вопросам комфорта и безопасности туристов. Мы лично проходим намеченные маршруты, поэтому уверены в них, и с удовольствием рекомендуем своим гостям.',
-    'Нашa цель – создать для вас настоящее приключение, которое подарит самые яркие эмоции и запомнится надолго.',
+    'Более 10 лет мы занимаемся организацией туров и экспедиций на плато Путорана и знаем, чем удивить самого искушенного путешественника.',
+    'Самые известные достопримечательности и места, где практически не ступала нога человека, мощный трекинг для выносливых и полёты на вертолётах, северная гастрономия от шеф-повара и гриль-пикники у водопадов — мы устроим для вас настоящее приключение в дикой природе с максимальным комфортом и эксклюзивным сервисом.',
+    'Мы работаем с корпоративными и частными группами, организуем персональные туры с индивидуальной программой, гастрономические и ивент-туры.',
+    'Можете быть уверены, что путешествие в затерянный мир плато Путорана вместе с нами подарит вам самые яркие эмоции и запомнится надолго.',
     'Антон Лысов',
     'Основатель и руководитель компании «Территория Путорана»'
   ],
-  img: "./img/anton.jpg"
+  img: "./img/anton2.jpg"
 }
 
 const textFewImages = {
@@ -53,10 +51,7 @@ export default async function Home() {
       <TextWord text = {textWord.text} word = {textWord.word}/>
       <CardsBlock tours = {tours} />
       <TextOneImage textOneImage = {textOneImage} />
-      <OneCardBlock />
-      <VideoBlock src = './img/main.mp4#t=0.001' h2Text = 'Покори Север с нами!'/>
-      <TextFewImages textFewImages = {textFewImages} />
-      <TextOneImage2 />
+      <VideoBlock src = './img/putorana.m4v#t=0.001' h2Text = 'Покори Север с нами!'/>
       <RequestBlock bgImage = {'/img/main_req.jpg'}  h2Text = {'Есть вопросы?'} h3Text = {"Оставьте заявку и мы обязательно свяжемся с вами!"} />
    </main>
   );
