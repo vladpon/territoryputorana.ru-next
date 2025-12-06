98a46cd3e4c7e4da9096b99b32c6371ce71b0e43
Vladislav Ponomarev <2874787@gmail.com>
2025-12-06 09:32:37 +0700

    react update

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -149,14 +149,14 @@ const Header = () => {
         <div className = {styles.header__contacts}>
           <a href="tel:+79039299383">+7 (903) 929-93-83</a>
           <div className = {styles.header__icons}>
-              <a href="https://vk.cc/cmIUok"><img className = {styles.header__icon} src="https://territoryputorana.ru/img/vk_ico.png"></img></a>
-              <a href="https://wa.me/79039299383"><img className = {styles.header__icon} src="https://territoryputorana.ru/img/wa_ico.png"></img></a>
-              <a href="https://t.me/territoryputorana"><img className = {styles.header__icon} src="https://territoryputorana.ru/img/tlgrm_ico.png"></img></a>
+              <a href="https://vk.cc/cmIUok"><img className = {styles.header__icon} src="/img/vk_ico.png"></img></a>
+              <a href="https://wa.me/79039299383"><img className = {styles.header__icon} src="/img/wa_ico.png"></img></a>
+              <a href="https://t.me/territoryputorana"><img className = {styles.header__icon} src="/img/tlgrm_ico.png"></img></a>
           </div>
         </div> 
         <div className = {styles.header__home}>
             <Link href = '/' onClick={() => setBurgermenuActive(false)}>
-                <img src='https://territoryputorana.ru/img/home.png'></img>
+                <img src='/img/home.png'></img>
             </Link>
         </div>
       </div>
